e378a41e2d92247533d75bcb5557ee4187ce577d
Michael Bestas <mikeioannina@gmail.com>
2015-09-29 18:47:51 +0100

    huashan: Disable dex2oat watchdog
    Low RAM devices are too slow to compile some very big APKs within the
    6 mins limit so we get the "Optimizing apps" dialog on each boot and
    dex2oat never completes properly, slowing down the whole system.
    
    Change-Id: I217ac9d41f3055bcb999674968927d88131e32d7

--- huashan.mk
+++ huashan.mk
@@ -305,7 +305,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.checkjni=false \
 	dalvik.vm.dex2oat-filter=everything \
-	dalvik.vm.dex2oat-flags=--no-watch-dog \
 	dalvik.vm.image-dex2oat-filter=everything \
 	dalvik.vm.isa.arm.features=lpae,div \
 	ro.kernel.android.checkjni=0
