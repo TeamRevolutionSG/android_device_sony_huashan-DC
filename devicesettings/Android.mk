aec654c31632376d80999121cf03e1e6153f64e6
AdrianDC <radian.dc@gmail.com>
2015-10-18 19:32:09 +0200

    TEMP: huashan: Implement Doze Sensors detection
     * Add gestures panel to the DeviceSettings
    
     * Features :
       - Hand wave Doze if device is not face down
       - Pick-up Doze if was face down or taken in hand
       - Pocket Doze if device unstowed vertically
       - Proximity Wake if device is not face down
    
     * Sensors :
       - Proximity sensor used for all the features
       - Accelerometer and Magnetometer used for the orientation
       - Accelerometer put in idle for the Pick-up with face up
         - This sensor will go in deep-sleep with the phone
    
      * Changes depends on :
           huashan: Implement Glove Mode service
           Doze kernel support
    
    Change-Id: I33fdb04d2131acd284c76993a69fdb342c3a39b0
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -6,7 +6,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform.sdk
+    org.cyanogenmod.platform.sdk \
+    org.cyanogenmod.platform.internal
 
 LOCAL_PACKAGE_NAME := DeviceSettings
 LOCAL_CERTIFICATE := platform
